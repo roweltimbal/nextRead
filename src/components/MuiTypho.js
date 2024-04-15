be68237e5d2757ce51150cd74b94f00441249cc5
PracticeBetter <practice_practice25@yahoo.com>
2024-04-15 10:56:00 -0400

    initial commit

---
+++ src/components/MuiTypho.js
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+
+export const MuiTypo = () => {
+  return (
+    <div>
+      <Typography variant='h1'>Next Read</Typography>
+      <Typography variant='h3'>Choose Your Next Read</Typography>
+    </div>
+  );
+};
---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { MuiTypo } from "../components/MuiTypho";
+
+function App() {
+  return (
+    <div className="App">
+      <MuiTypo />
+    </div>
+  );
+}
+
+export default App;
